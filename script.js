199f566ef4d9b4d650b5bc28230794c7e43f7b0c
MikeyKarina <michaelkarina90@gmail.com>
2025-04-07 01:17:15 +0300

    healings

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// // Array of image paths
-// const images = [
-//    "Slide-1.jpg", "Slide-2.jpg", "Slide-3.jpg", "Slide-4.jpg", "Slide-5.jpg", "Slide-6.jpg", "Slide-7.jpg", "Slide-8.jpg", "Slide-9.jpg", "Slide-10.jpg", "Slide-11.jpg", "Slide-12.jpg", "Slide-13.jpg", 
-// ];
-
 let currentIndex = 0; // Track the current image index
 const slideshowElements = document.querySelectorAll('.slideshow'); // Get all images with the class 'slideshow'
 
